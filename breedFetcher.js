8370758e2fb1394e905c4ee863d4e4bb82490087
Ana Carolina K <anacko@gmail.com>
2021-09-23 17:07:38 -0400

    refactor to module

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,36 +1,47 @@
 const request = require('request');
 
-const userInput = process.argv.slice(2);
-if (userInput.length === 0) {
-  console.log("Attention: Insert breed to know more about it! For a list of breeds, insert breeds");
-  process.exit();
-}
+const fetchBreedDescription = function(breedName, callback) {
 
-request('https://api.thecatapi.com/v1/breeds', function(error, response, body) {
+  request('https://api.thecatapi.com/v1/breeds', function(requestError, response, body) {
+  
+    let error = null;
 
-  if (error) {
-    console.error('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    return;
-  }
+    if (requestError) {
+      callback([requestError, response], null);
+      return;
+    }
 
-  const data = JSON.parse(body);
+    if (!breedName) {
+      error = "Invalid input. For a list of available breeds, use list.";
+      callback(error, null);
+      return;
+    }
 
-  let catNames = [];
-  for (const cat of data) {
-    catNames.push(cat.name);
-  }
+    const data = JSON.parse(body);
+  
+    let catNames = [];
+    for (const cat of data) {
+      catNames.push(cat.name.toLowerCase());
+    }
 
-  if (userInput[0].toLowerCase() === "breeds") {
-    console.log(catNames);
-    return;
-  }
+    breedName = breedName.toLowerCase();
+  
+    if (breedName === "list") {
+      const description = catNames;
+      callback(null, description);
+      return;
+    }
+  
+    if (catNames.includes(breedName)) {
+      const catPos = catNames.indexOf(breedName);
+      const description = data[catPos].description;
+      callback(null, description);
+    } else {
+      error = "Breed not available. For a list, use list.";
+      callback(error, null);
+      return;
+    }
+  });
+};
 
-  if (catNames.includes(userInput[0])) {
-    const catPos = catNames.indexOf(userInput[0]);
-    console.log(data[catPos].description);
-  } else {
-    console.log("Cat unavailable. To get a list of available cats, insert the option breeds.");
-  }
-
-});
+module.exports = { fetchBreedDescription };
